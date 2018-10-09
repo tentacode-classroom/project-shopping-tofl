17c53ed3ad89d1643c994d9739e5cfcf56f3143c
Tom F <thomas.flitterman@ynov.com>
2018-10-09 10:17:43 +0200

    Etape 8 (sans la relation ManyToMany pour l’instant)

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\RegistrationType;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/signup", name="registration")
+     */
+    public function index(Request $request)
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() AND $form->isValid()) {
+            $user = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('registration_validated');
+        }
+
+        return $this->render('registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/confirmation/confirm", name="registration_validated")
+     */
+    public function confirmation() {
+        // Renvoyer l'utilisateur à la page d'inscription (une fois l'inscription validée) tout en lui retirant la possibilité
+        // de rafraîchir la page pour envoyer plusieurs fois de suite les informations dans la base de données
+        return $this->redirectToRoute('registration');
+    }
+}
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom de famille'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse mail : '
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe : '
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Inscription'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
